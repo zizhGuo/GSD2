8bf10555758bee63358cc9c109acb2aeec16a841
zg2808 <zg2808@IGM2550-06>
2017-08-30 10:49:21 -0400

    nothing changed actually

---
+++ Creatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevationMaster
+{
+    abstract class Creatures
+    {
+
+    }
+    class Ayarm : Creatures
+    {
+
+    }
+    class Bixam : Creatures
+    {
+
+    }
+}
