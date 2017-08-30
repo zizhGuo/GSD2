d310d9cda0fb846127c5d72b786222de0aa37091
zg2808 <zg2808@IGM2550-06>
2017-08-30 14:03:40 -0400

    undate mate function

--- AlienZoo/AlienZoo/Program.cs
+++ AlienZoo/AlienZoo/Program.cs
@@ -9,28 +9,58 @@ namespace AlienZoo
     class AlienBreedingSims
     {
         int days;
-        public AlienBreedingSims(int days)
+        public AlienBreedingSims()
         {
-            Console.WriteLine("Please input the simulation days: ");
-            this.days = days;
-        }
-        //int Randomclas()
-        //{
-        //    Random rand = new Random();
-        //    if (rand.NextDouble() >= 0.5) ;
-        //}
 
+        }
         public static void Sims()
         {
             bool isSimsOn = true;
+            int days = 0;
             Creatures[] Creature = new Creatures[15];
-            // List <T>
             try
             {
                 int index = 0;
+
+                Console.WriteLine("Please input the simulation days: ");
+                days = int.Parse(Console.ReadLine());
+
+
                 while (isSimsOn)
                 {
-                    // Creature[index] = Random
+                    Random rand = new Random();
+                    if (rand.NextDouble() > 0.5)
+                    {
+                        Ayam freshman = new Ayam();
+                        Creature[index] = freshman;
+                    }
+                    else
+                    {
+                        Bixam freshman = new Bixam();
+                        Creature[index] = freshman;
+                    }
+
+
+                    if (index > 0)
+                    {
+                        for (int i = 0; i < 15; i++)
+                        {
+                            var typea = Creature[i].GetType();
+                            if (Creature[i].IsAllowMate == true && typea == Creature[index].GetType() && )
+                            {
+                                
+                            }
+                        }
+                    }
+
+
+                    index++;
+
+
+                    if (index == 14)
+                    {
+                        break;
+                    }
                 }
             }
             catch (Exception e)
@@ -41,33 +71,25 @@ namespace AlienZoo
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Please input the simulation days: ");
-            new AlienBreedingSims(int.Parse(Console.ReadLine()));
             Sims();
         }
     }
     abstract class Creatures
     {
-
-    }
-    class Ayam : Creatures
-    {
         public bool IsAllowMate
         {
             get; set;
         }
+    }
+    class Ayam : Creatures
+    {
         public float matingChance
         {
             get { return 1; }
         }
-
     }
     class Bixam : Creatures
     {
-        public bool IsAllowMate
-        {
-            get; set;
-        }
         public float matingChance
         {
             get { return 1 / 3; }
